3e6ac260a6ed1467ca3dd8e6559444bf4d37b184
Francesco Adami <francescoadami9@gmail.com>
2025-06-27 15:24:10 +0200

    fixes

--- Assets/_Scripts/Candly/LightActivator.cs
+++ Assets/_Scripts/Candly/LightActivator.cs
@@ -4,7 +4,6 @@ public class LightActivator : MonoBehaviour
 {
     private void OnTriggerStay(Collider other)
     {
-        Debug.Log("Candly ha incontrato: " + other.name);
         if (other.TryGetComponent<ILightActivator>(out ILightActivator activator))
         {
             activator.OnLightActivate();
